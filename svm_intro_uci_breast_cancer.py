18baf72bb9910d4dd2beb83170b4f06f55df79e5
Hasnain Hossain <has9h2@gmail.com>
2020-04-11 03:18:25 +0600

    Confusion matrix added

--- svm_intro_uci_breast_cancer.py
+++ svm_intro_uci_breast_cancer.py
@@ -1,8 +1,11 @@
 import numpy as np
 from sklearn import preprocessing, model_selection, neighbors, svm
+from sklearn.metrics import classification_report, confusion_matrix
 import pandas as pd
 
-df = pd.read_csv('../Datasets/breast-cancer-wisconsin.data')
+df = pd.read_csv('../Datasets/Breast-Cancer/breast-cancer-wisconsin.data')
+
+# Data cleanup
 df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
 
@@ -22,7 +25,16 @@ clf.fit(X_train, y_train)
 acc = clf.score(X_test, y_test)
 print(acc)
 
-example_measures = np.array([[10, 5, 5, 4, 7, 6, 7, 2, 2], [8, 3, 4, 9, 2, 6, 7, 4, 5]])
-example_measures = example_measures.reshape(len(example_measures), -1)
-predict = clf.predict(example_measures)
+# Simple sample to predict
+# example_measures = np.array([[10, 5, 5, 4, 7, 6, 7, 2, 2], [8, 3, 4, 9, 2, 6, 7, 4, 5]])
+# example_measures = example_measures.reshape(len(example_measures), -1)
+
+# Predicting the test set (Validation would be good here)
+predict = clf.predict(X_test)
+
+# Checking class labels
 print(predict)
+
+# Reports
+print(confusion_matrix(y_test, predict))
+print(classification_report(y_test, predict))
